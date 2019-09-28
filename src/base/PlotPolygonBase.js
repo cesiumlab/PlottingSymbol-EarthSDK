3ea5d56b515bc5263ab9995c586be4b400433c17
vtxf <vtxf@qq.com>
2019-09-28 13:25:53 +0800

    add base

---
+++ src/base/PlotPolygonBase.js
@@ -0,0 +1,63 @@
+class PlotPolygonBase extends XE.Obj.PlotBase {
+    constructor(earth, guid) {
+        super(earth, guid);
+
+        this._onlyMove = false;
+        this._fixedPositionsNum = undefined;
+
+        XE.MVVM.extend(this, {
+            _polygonShow: false,
+        });
+
+        this._polygon = new XE.Obj.Polygon(earth);
+        this.disposers.push(() => {
+            this._polygon = this._polygon && this._polygon.destroy();
+        });
+        this._polygon.show = this._polygonShow && this.show;
+
+        const updatePolygon = () => {
+            this._polygon.show = this._polygonShow && this.show;
+            this._polygon.width = this.width;
+            this._polygon.color = this.color;
+            this._polygon.ground = this.ground;
+
+            this._polygon.outline.width = this.outlineWidth;
+            this._polygon.outline.color = this.outlineColor;
+            this._polygon.outline.show = this.outlineShow;
+        }
+
+        updatePolygon();
+        this.disposers.push(reaction(() => {
+            return {
+                show: this._polygonShow && this.show,
+                width: this.width,
+                color: [...this.color],
+                ground: this.ground,
+                outlineWidth: this.outlineWidth,
+                outlineColor: this.outlineColor,
+                outlineShow: this.outlineShow,
+            }
+        }, () => updatePolygon()));
+    }
+
+    get onlyMove() {
+        return this._onlyMove;
+    }
+
+    get fixedPositionsNum() {
+        return this._fixedPositionsNum;
+    }
+}
+
+PlotPolygonBase.defaultOptions = {
+    positions: [],
+    show: true,
+    color: [0, 1, 0, 0.1],
+    outlineWidth: 2.0,
+    outlineColor: [0, 1, 0, 1],
+    outlineShow: true,
+};
+        
+PlotPolygonBase.registerType(PlotPolygonBase, 'PlotPolygonBase');
+
+export default PlotPolygonBase;
---
+++ src/base/PlotPolylineBase.js
@@ -0,0 +1,55 @@
+class PlotPolylineBase extends XE.Obj.PlotBase {
+    constructor(earth, guid) {
+        super(earth, guid);
+
+        this._onlyMove = false;
+        this._fixedPositionsNum = undefined;
+
+        XE.MVVM.extend(this, {
+            _polylineShow: false,
+        });
+
+        this._polyline = new XE.Obj.Polyline(earth);
+        this.disposers.push(() => {
+            this._polyline = this._polyline && this._polyline.destroy();
+        });
+        this._polyline.show = this._polylineShow && this.show;
+
+        const updatePolyline = () => {
+            this._polyline.show = this._polylineShow && this.show;
+            this._polyline.width = this.width;
+            this._polyline.material.XbsjColorMaterial.color = this.color;
+            this._polyline.ground = this.ground;
+        }
+
+        updatePolyline();
+        this.disposers.push(reaction(() => {
+            return {
+                show: this.show,
+                polylineShow: this._polylineShow,
+                width: this.width,
+                color: [...this.color],
+                ground: this.ground,
+            }
+        }, () => updatePolyline()));
+    }
+
+    get onlyMove() {
+        return this._onlyMove;
+    }
+
+    get fixedPositionsNum() {
+        return this._fixedPositionsNum;
+    }
+}
+
+PlotPolylineBase.defaultOptions = {
+    positions: [],
+    show: true,
+    width: 5.0,
+    color: [0, 1, 0, 1],
+};
+        
+PlotPolylineBase.registerType(PlotPolylineBase, 'PlotPolylineBase');
+
+export default PlotPolylineBase;
