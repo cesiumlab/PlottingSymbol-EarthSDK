958d936b7d3f231ff3fb4d19dad6562f95f8cd34
vtxf <vtxf@qq.com>
2021-01-20 15:50:42 +0800

    add holeRadius

--- src/GeoCircle.js
+++ src/GeoCircle.js
@@ -3,6 +3,7 @@ import PlotPolygonBase from './base/PlotPolygonBase';
 const Tool = XE.Tool;
 
 const c_polygonPositions = new Array(360).fill(0).map(e => [0, 0, 0]);
+const c_pgPositions = [];
 
 class GeoCircle extends PlotPolygonBase {
   constructor(earth, guid) {
@@ -11,14 +12,14 @@ class GeoCircle extends PlotPolygonBase {
     this._onlyMove = true;
     this._fixedPositionsNum = 2;
 
-    this._pgPositions = [];
 
     this.disposers.push(
       XE.MVVM.watch(
-        () => {
-          return [...this.positions.map(e => [...e])];
-        },
-        positions => {
+        () => ({
+          positions: [...this.positions.map(e => [...e])],
+          holeRadius: this.holeRadius,
+        }),
+        ({positions, holeRadius}) => {
           const l = positions.length;
 
           if (l < 2) {
@@ -35,21 +36,42 @@ class GeoCircle extends PlotPolygonBase {
 
           const slice = 360;
 
-          for (let i = 0; i < slice; i++) {
-            Tool.Math.geoMove(
-              positions[0],
-              i * Math.PI/180,
-              d,
-              c_polygonPositions[i],
-            );
+          {
+            for (let i = 0; i < slice; i++) {
+              Tool.Math.geoMove(
+                positions[0],
+                i * Math.PI/180,
+                d,
+                c_polygonPositions[i],
+              );
+            }
+  
+            c_pgPositions.length = 0;
+            c_polygonPositions.forEach(e => {
+              c_pgPositions.push(e[0], e[1]);
+            });
+  
+            this._polygon.positions = c_pgPositions;
           }
 
-          this._pgPositions.length = 0;
-          c_polygonPositions.forEach(e => {
-            this._pgPositions.push(e[0], e[1]);
-          });
+          if (this.holeRadius > 0 && this.holeRadius < d) {
+            for (let i = 0; i < slice; i++) {
+              Tool.Math.geoMove(
+                positions[0],
+                i * Math.PI/180,
+                this.holeRadius,
+                c_polygonPositions[i],
+              );
+            }
+  
+            c_pgPositions.length = 0;
+            c_polygonPositions.forEach(e => {
+              c_pgPositions.push(e[0], e[1]);
+            });
+  
+            this._polygon.holePositions = c_pgPositions;
+          }
 
-          this._polygon.positions = this._pgPositions;
           this._polygon.height = positions[0][2];
 
           this._polygonShow = true;
@@ -58,6 +80,10 @@ class GeoCircle extends PlotPolygonBase {
     );
   }
 }
+
+GeoCircle.defaultOptions = {
+  holeRadius: 0,
+};
 
 GeoCircle.registerType(GeoCircle, "GeoCircle");
 
